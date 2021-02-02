1475a963939dd5848c0d2134235c62e5876196bf
LYP <1691608003@qq.com>
2021-02-02 17:06:25 +0800

    增加对yolov5 4.0的支持

--- models/experimental.py
+++ models/experimental.py
@@ -134,7 +134,10 @@ def attempt_load(weights, map_location=None):
     model = Ensemble()
     for w in weights if isinstance(weights, list) else [weights]:
         attempt_download(w)
-        model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
+        try:
+            model.append(torch.load(w, map_location=map_location)['model'].float().fuse().eval())  # load FP32 model
+        except Exception:
+            model.append(torch.load(w, map_location=map_location).float().fuse().eval())  # load FP32 model
 
     # Compatibility updates
     for m in model.modules():
